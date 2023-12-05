8641bc9eaaf5e7c901e823740aec35b016605222
Beater <109640384+NTH2k4@users.noreply.github.com>
2023-12-06 00:50:58 +0700

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -7,12 +7,10 @@
 #include<queue>
 #include<cstring>
 #include<algorithm>
-#include <boost/multiprecision/cpp_int.hpp>
 #define ll long long
 #define ull unsigned long long
 #define sync ios_base::sync_with_stdin(0); cin.tie(0); cout.tie(0);
 #define loop(n) for(int i=0; i<n; i++)
-#define bigint(str) boost::multiprecision::cpp_int num(str);
 
 using namespace std;
 const int IMAX = 1e9+7;
