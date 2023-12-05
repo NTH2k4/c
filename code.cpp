add1f8cc7178e8dedbba1f249451bc50edec6c87
Beater <109640384+NTH2k4@users.noreply.github.com>
2023-12-06 06:35:23 +0700

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -7,11 +7,37 @@
 #include<queue>
 #include<cstring>
 #include<algorithm>
+#include<bits/stc++.h>
 #define ll long long
 #define ull unsigned long long
 #define sync ios_base::sync_with_stdin(0); cin.tie(0); cout.tie(0);
 #define loop(n) for(int i=0; i<n; i++)
+#define read freopen("in.txt","r",stdin); 
+#define write freopen("out.txt","w",stdout);
+//priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<pair<ll,int>>> pq;
+/*void Dijkstra(int s){
+    fill(dist + 1, dist + n + 1, INF);
+    dist[s] = 0;
+    priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<pair<ll,int>>> pq;
+    pq.push({0, s});
+    while(!pq.empty()){
+        int u = pq.top().second;
+        pq.pop();
+        if(mark[u]) continue;
+        mark[u] = true;
+        for(auto e : adj[u]){
+            int v = e.first;
+            ll w = e.second;
+            if(dist[v] > dist[u] + w){
+                dist[v] = dist[u] + w;
+                pq.push({dist[v], v});
+            }
+        }
+    }
+}*/
 
 using namespace std;
 const int IMAX = 1e9+7;
 const int MAX = 1e7+5;
+const ll INF = 1e18;
+
