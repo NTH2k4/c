0320a5ad41a2c061d9dac9f6728284776250d968
Beater <109640384+NTH2k4@users.noreply.github.com>
2023-12-05 22:14:13 +0700

    C++

---
+++ code.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<map>
+#include<set>
+#include<unordered_map>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<cstring>
+#include<algorithm>
+#define ll long long
+#define ull unsigned long long
+#define sync ios_base::sync_with_stdin(0); cin.tie(0); cout.tie(0);
+#define loop(n) for(int i=0; i<n; i++)
+
+using namespace std;
+const int IMAX = 1e9+7;
+const int MAX = 1e7+5;
